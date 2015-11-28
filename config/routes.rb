9c82cde2fbb389b1b9b9f9ca47740e4616341e69
Jordan Hudgens <jordan@wow.com>
2015-11-27 20:05:31 -0600

    Implemented scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   resources :accounts
   get 'admin/dashboard'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127233150) do
+ActiveRecord::Schema.define(version: 20151128001950) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.string   "payment_status"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "authors", force: :cascade do |t|
